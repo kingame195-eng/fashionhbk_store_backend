bb2b7fa2729b32733582b9ef49fe5ff397e3a941
kingame195-eng <kingame195@gmail.com>
2026-02-06 08:38:07 +0700

    fix: update pre-save hook in User model to handle async errors correctly; remove deprecated API test report

--- src/models/User.js
+++ src/models/User.js
@@ -100,14 +100,21 @@ userSchema.virtual("fullName").get(function () {
 });
 
 // Hash password before saving
-userSchema.pre("save", async function () {
+// Note: In Mongoose 9.x, async pre-save hooks should NOT use next()
+// They should either return a value or throw an error
+userSchema.pre("save", async function (next) {
   // Only hash if password is modified
   if (!this.isModified("password")) {
-    return;
+    return next();
   }
 
-  const salt = await bcrypt.genSalt(12);
-  this.password = await bcrypt.hash(this.password, salt);
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Compare password method
